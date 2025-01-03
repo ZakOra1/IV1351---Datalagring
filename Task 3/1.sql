895c5f197cb21143df52d20267d2029269641433
Zak Ora <zak.ora.student@gmail.com>
2025-01-03 12:06:28 +0100

    Changed task 1

--- Task 3/1.sql
+++ Task 3/1.sql
@@ -1,6 +1,5 @@
 SELECT 
     TO_CHAR(scheduled_time, 'Mon') AS month,
-    EXTRACT(MONTH FROM scheduled_time) AS month_number,
     COUNT(*) AS total_lessons,
     SUM(CASE WHEN lesson_type = 'Individual' THEN 1 ELSE 0 END) AS individual_lessons,
     SUM(CASE WHEN lesson_type = 'Group' THEN 1 ELSE 0 END) AS group_lessons,
@@ -8,4 +7,4 @@ SELECT
 FROM lesson
 WHERE EXTRACT(YEAR FROM scheduled_time) = 2025
 GROUP BY EXTRACT(MONTH FROM scheduled_time), TO_CHAR(scheduled_time, 'Mon')
-ORDER BY month_number;
+ORDER BY EXTRACT(MONTH FROM scheduled_time);
